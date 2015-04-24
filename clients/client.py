db23146c61f4197a2497067d88945b1e42597c3c
Davide Piscia <dpiscia@davides-air.ti00a1.pcb.ub.es>
2015-04-24 09:14:22 +0200

    validate ticket

--- clients/client.py
+++ clients/client.py
@@ -1,9 +1,18 @@
 import requests
+import urllib
 service='http://localhost:8081'
-rep = requests.post("http://10.3.231.71:8080/cas/v1/tickets",data="username=jleleu&password=jleleu&service=prova")
+username="rdconnect-test"
+password="1234.abcd"
+rep = requests.post("http://10.3.231.71:8080/cas/v1/tickets",data="username="+username+"&password="+password+"&service=prova")
 location=rep.headers.get('location')
 tgt = location[location.rfind('/') + 1:]
 
 params = urllib.urlencode({'service': service })
 respo = requests.post("http://10.3.231.71:8080/cas/v1/tickets/"+tgt,data=urllib.urlencode({'service': service }))
-rep = requests.post("http://10.3.231.71:8080/cas/login",data="username=jleleu&password=jleleu&service=http://10.3.231.71:8080/securestaff")
+st = respo.text
+validate = requests.post("http://10.3.231.71:8080/cas/serviceValidate?ticket="+st+"&service="+service)
+
+#rep = requests.post("http://10.3.231.71:8080/cas/login",data="username=jleleu&password=jleleu&service=http://10.3.231.71:8080/securestaff")
+
+
+delete = requests.delete("http://10.3.231.71:8080/cas/v1/tickets/"+tgt)
